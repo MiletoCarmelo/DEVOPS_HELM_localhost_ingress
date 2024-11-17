db8affa9ee7a8524a1787d26ed2c2d44a08db130
MiletoCarmelo <carmelo.mileto@gmail.com>
2024-11-17 19:01:37 +0100

    add

--- scripts/creer_secret_tailscale.sh
+++ scripts/creer_secret_tailscale.sh
@@ -17,6 +17,13 @@ OAUTH_CLIENT_ID=$(grep OAUTH_CLIENT_ID .env | cut -d '=' -f2)
 OAUTH_CLIENT_SECRET=$(grep OAUTH_CLIENT_SECRET .env | cut -d '=' -f2)
 TAILSCALE_KEY=$(grep TAILSCALE_KEY .env | cut -d '=' -f2)
 
+# Créer le secret pour TS_AUTHKEY
+echo "🔒 Création du secret Tailscale Auth..."
+kubectl create secret generic tailscale-auth \
+  --namespace=${NAMESPACE} \
+  --from-literal=TS_AUTHKEY=${TAILSCALE_KEY} \
+  --dry-run=client -o yaml | kubectl apply -f -
+
 # Installation de Tailscale avec Helm
 echo "🔒 Installation de Tailscale..."
 
@@ -24,5 +31,4 @@ helm upgrade --install tailscale-operator tailscale/tailscale-operator \
   --namespace=${NAMESPACE} \
   --create-namespace \
   --set-string ts.oauth.clientId=${TAILSCALE_OAUTH_CLIENT_ID} \
-  --set-string ts.oauth.clientSecret=${TAILSCALE_OAUTH_CLIENT_SECRET} \
-  --set-string ts.oauth.tsKey=${TAILSCALE_KEY} 
+  --set-string ts.oauth.clientSecret=${TAILSCALE_OAUTH_CLIENT_SECRET} 
