5c2073599971efc2aeb9976e2a8a5ccf91a6d5df
MiletoCarmelo <carmelo.mileto@gmail.com>
2024-12-15 22:03:24 +0100

    add

--- scripts/creer_secret_tailscale.sh
+++ scripts/creer_secret_tailscale.sh
@@ -4,23 +4,28 @@
 NAMESPACE="tailscale"
 NAMESPACE_localhost="metallb-system"
 
+# Obtenir le chemin du répertoire du script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Lecture des variables depuis .env
-TS_CLIENT_ID=$(grep TS_CLIENT_ID .env | cut -d '=' -f2)
-TS_CLIENT_SECRET=$(grep TS_CLIENT_SECRET .env | cut -d '=' -f2)
-TS_AUTHKEY=$(grep TS_AUTHKEY .env | cut -d '=' -f2)
-TS_DEST_IP=$(grep TS_DEST_IP .env | cut -d '=' -f2)
-TS_ROUTES=$(grep TS_ROUTES .env | cut -d '=' -f2)
-TS_HOSTNAME=$(grep TS_HOSTNAME .env | cut -d '=' -f2)
+TS_CLIENT_ID=$(grep TS_CLIENT_ID "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
+TS_CLIENT_SECRET=$(grep TS_CLIENT_SECRET "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
+TS_AUTHKEY=$(grep TS_AUTHKEY "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
+TS_DEST_IP=$(grep TS_DEST_IP "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
+TS_ROUTES=$(grep TS_ROUTES "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
+TS_HOSTNAME=$(grep TS_HOSTNAME "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
+
 
 # Lecture de l'IP Tailscale et ajout du masque CIDR si nécessaire
-TS_IP=$(grep TS_IP .env | cut -d '=' -f2)
+TS_IP=$(grep TS_IP "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
+TAILSCALE_IP=$(grep TS_IP "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
 if [[ ! $TS_IP =~ /[0-9]{1,2}$ ]]; then
     TS_IP="${TS_IP}/32"
     echo "ℹ️  Ajout du masque CIDR à l'adresse IP Tailscale: ${TS_IP}"
 fi
 
 # Lecture de l'IP Localhost - gestion spéciale pour les plages d'adresses
-LOCALHOST_IP=$(grep LOCALHOST_IP .env | cut -d '=' -f2)
+LOCALHOST_IP=$(grep LOCALHOST_IP "${SCRIPT_DIR}/../.env" | cut -d '=' -f2)
 if [[ $LOCALHOST_IP =~ "-" ]]; then
     # C'est une plage d'adresses, pas besoin d'ajouter /32
     echo "ℹ️  Utilisation de la plage d'adresses pour Localhost: ${LOCALHOST_IP}"
@@ -49,6 +54,7 @@ kubectl create secret generic ts-secrets \
     --from-literal=TS_ROUTES=${TS_ROUTES} \
     --from-literal=TS_HOSTNAME=${TS_HOSTNAME} \
     --from-literal=TS_IP=${TS_IP} \
+    --from-literal=TAILSCALE_IP=${TAILSCALE_IP} \
     --dry-run=client -o yaml | kubectl apply -f -
 
 # Création du secret LOCALHOST-SECRET
@@ -67,3 +73,11 @@ kubectl create secret generic operator-oauth \
     --dry-run=client -o yaml | kubectl apply -f -
 
 echo "✅ Configuration terminée"
+
+echo "  => TS_AUTHKEY ${TS_AUTHKEY}"
+echo "  => TS_DEST_IP ${TS_DEST_IP}"
+echo "  => TS_ROUTES ${TS_ROUTES}"
+echo "  => TS_AUTHKEY ${TS_AUTHKEY}"
+echo "  => TS_HOSTNAME ${TS_HOSTNAME}"
+echo "  => TS_IP ${TS_IP}"
+echo "  => TAILSCALE_IP ${TAILSCALE_IP}"
